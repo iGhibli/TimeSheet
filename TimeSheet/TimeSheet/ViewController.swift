23054bca3251c09498166152c42d6941cd812118
iGhibli <iGhibli@163.com>
2018-09-04 19:50:35 +0800

    Empty project.

---
+++ TimeSheet/TimeSheet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeSheet
+//
+//  Created by Lindada on 2018/9/4.
+//  Copyright © 2018年 iGhibli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
